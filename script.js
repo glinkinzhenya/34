eb140c1ea81e842d26fe5f715f629d7eed122d1c
glinkinzhenya <glinkinzhenya@gmail.com>
2023-01-14 20:26:17 +0200

    1

---
+++ script.js
@@ -0,0 +1,7 @@
+// У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg
+//  Вивести зображення з цієї папки, отримане випадковим чином(Math.random)
+
+const img = document.createElement("img");
+img.setAttribute("width", "100%"); 
+img.setAttribute("src", `image/${Math.round(Math.random() * (9 - 1) + 1)}.jpg`);
+document.body.append(img);
